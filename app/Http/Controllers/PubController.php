e6d6f429e7a9ac09ce95530c45036483ad782cac
Vito <lolozol80xd@gmail.com>
2024-01-08 09:01:54 +0100

    update

--- app/Http/Controllers/PubController.php
+++ app/Http/Controllers/PubController.php
@@ -7,5 +7,35 @@ use Illuminate\Support\Facades\DB;
 
 class PubController extends Controller
 {
-    //
+    public function getDrinks(){
+        $drinks = DB::select("select * from drinks;");
+        return $drinks;
+    }
+    public function getDrinkNames(){
+        $names = DB::select("select name from drinks;");
+        return $names;
+    }
+    public function getNameAndQuantity(){
+        $data = DB::select("select name,quantity_id from drinks;");
+        return $data;
+    }
+    public function getSelectedDrink(){
+        $drink = DB::select("select name from drinks where id = 3;");
+        return $drink;
+    }
+    public function getSelectedNumber(){
+        $number = DB::select("select name from drinks where amount = 25;");
+        return $number;
+    }
+    public function insertDrink(){
+        DB::insert("insert into drinks(name, amount, type_id, quantity_id) values (?,?,?,?)",[
+            "Borsodi",120,1,1
+        ]);
+    }
+    public function modifyDrink(){
+        DB::update("update drinks set name = ? where name = ?",["Soproni","Borsodi"]);
+    }
+    public function deleteDrink(){
+        DB::delete("delete from drinks where name = ?",["Soproni"]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PubController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get("/drinks",[PubController::class, "getDrinks"]);
+Route::get("/names",[PubController::class, "getDrinkNames"]);
+Route::get("/data",[PubController::class, "getNameAndQuantity"]);
+Route::get("/selected",[PubController::class, "getSelectedDrink"]);
+Route::get("/number",[PubController::class, "getSelectedNumber"]);
+Route::get("/insert",[PubController::class, "insertDrink"]);
+Route::get("/modify",[PubController::class, "modifyDrink"]);
+Route::get("/delete",[PubController::class, "deleteDrink"]);
